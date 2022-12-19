f7c8921b2da3d32d77fdac387015c31ce58301d7
re91na <113601683+re91na@users.noreply.github.com>
2022-12-19 14:12:45 +0500

    Task5 updated

--- task5.py
+++ task5.py
@@ -10,18 +10,6 @@ def fill_list_random(n, a, b):
     return list
 
 
-# def list_asc(list):
-#     mid = len(list) // 2
-#     j = 0
-#     while j <= mid:
-#         index_a = list[len(list)-1-j]
-#         temp = list[j]
-#         list[j] = list[index_a]
-#         list[index_a] = temp
-#         j += 1
-#     return list
-
-
 len = int(input("Сколько будет элементов в списке? "))
 a = int(input("Нижняя граница диапазона: "))
 b = int(input("Верхняя граница диапазона: "))
